764984e6610f39308e4c3196f26e6dd05aab83e1
phoebeharmon <phoebeharmon@gmail.com>
2019-11-30 17:22:31 -0600

    Update main to use DateCalculator methods

--- fantastic-finale-phoebeharmon/src/main.cpp
+++ fantastic-finale-phoebeharmon/src/main.cpp
@@ -18,25 +18,27 @@ int main( ){
     //https://web.housing.illinois.edu/MobileDining2/WebService/Search.aspx?t=json&k=7A828F94-620B-4EE3-A56F-328036CC3C04&id=1&from=2019-12-02&to=2019-12-02
     std::string url_base = "https://web.housing.illinois.edu/MobileDining2/WebService/Search.aspx?t=json&k=7A828F94-620B-4EE3-A56F-328036CC3C04&id=";
     std::string url_id = "1";
-    std::string url_date_one = "2019-12-02";
-    std::string url_date_two = "2019-12-02";
-    
-    std::string url_string = url_base + url_id + "&from=" + url_date_one + "&to=" + url_date_two;
+    //std::string url_date_one = "2019-12-02";
+    //std::string url_date_two = "2019-12-02";
     
     // Get date information
     DateCalculator calculator;
-    std::cout << calculator.GetWeekLaterDate(calculator.GetCurrentDate());
+    std::vector<std::string> date = calculator.GetDates();
+    std::string url_date_one = date[0];
+    std::string url_date_two = date[1];
     
+    std::string url_string = url_base + url_id + "&from=" + url_date_one + "&to=" + url_date_two;
+    std::cout << url_string;
     
     // Convert url string to vector of Item objects
     DataProcessor processor;
     std::string url_content = processor.ReadUrl(url_string);
     nlohmann::json json_object = processor.ConvertStringToJson(url_content);
     std::vector<Item> items = processor.ConvertJsonToItems(json_object);
-    /*
+    
     for (Item element : items) {
         std::cout << element.meal << "\t";
         std::cout << element.course << "\t\t";
         std::cout << element.formal_name << std::endl;
-    }*/
+    }
 }
