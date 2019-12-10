65c8d8cab9119d9d495bf9352ee41302c4f7168b
phoebeharmon <phoebeharmon@gmail.com>
2019-12-10 01:44:49 -0600

    Create stub for FavoritesProcessor class

---
+++ fantastic-finale-phoebeharmon/src/favoritesprocessor.cpp
@@ -0,0 +1,20 @@
+//
+//  favoritesprocessor.cpp
+//  fantastic-finale-phoebeharmon
+//
+//  Created by Phoebe Harmon on 12/10/19.
+//
+
+#include "favoritesprocessor.hpp"
+
+std::vector<Item> FavoritesProcessor::GetFutureMenu() {
+    
+}
+
+std::map<Item, std::vector<std::string>> FavoritesProcessor::FindFavoritesFutureInformation() {
+    
+}
+
+std::string FavoritesProcessor::GetChart() {
+    
+}
---
+++ fantastic-finale-phoebeharmon/src/favoritesprocessor.hpp
@@ -0,0 +1,25 @@
+//
+//  favoritesprocessor.hpp
+//  fantastic-finale-phoebeharmon
+//
+//  Created by Phoebe Harmon on 12/10/19.
+//
+
+#ifndef favoritesprocessor_hpp
+#define favoritesprocessor_hpp
+
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <vector>
+#include "item.hpp"
+
+#endif /* favoritesprocessor_hpp */
+class FavoritesProcessor {
+public:
+    std::vector<Item> GetFutureMenu();
+    std::map<Item, std::vector<std::string>> FindFavoritesFutureInformation();
+    std::string GetChart();
+private:
+    std::map<int, std::string> dining_hall_ids{{1, "Ikenberry"}, {2, ""}, {3, ""}, {4, ""}, {5, ""}, {6, ""}, {7, ""}, {8, ""}, {9, ""}, {10, ""}, {11, ""}, {12, ""}, {13, ""}, {14, ""}, {15, ""}, {16, ""}, {17, ""}, {18, ""}, {19, ""}, {20, ""}, {21, ""}};
+};
